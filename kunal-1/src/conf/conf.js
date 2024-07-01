42607fb56ca9e203f5ede5d3670737255c7f56b1
Hitesh Kumar Rajpurohit <hiteshkumardrajpurohit@gmail.com>
2024-07-01 09:15:38 +0530

    c-1

---
+++ kunal-1/src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf ={
+ appWriteUrl:String(process.env.APPWRITE_URL),
+ appWriteProjectId:String(process.env.APPWRITE_PROJECT_ID),
+ appWriteDatabaseId:String(process.env.APPWRITE_DATABASE_ID),
+ appWriteCollectionId:String(process.env.APPWRITE_COLLECTION_ID),
+ appWriteBucketId:String(process.env.APPWRITE_BUCKET_ID) ,   
+}
+
+export default conf;
